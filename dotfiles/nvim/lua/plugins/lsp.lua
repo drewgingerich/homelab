c506a750a7dc0fbf5b9623069231f8e012ed0644
Drew Gingerich <drew@elsewhere.space>
2025-04-20 16:15:01 +0200

    nvim: configure CSS language server

--- dotfiles/nvim/lua/plugins/lsp.lua
+++ dotfiles/nvim/lua/plugins/lsp.lua
@@ -5,6 +5,7 @@ return {
 			local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
 			local lspconfig = require("lspconfig")
+			lspconfig.cssls.setup({ capabilities = capabilities })
 			lspconfig.gleam.setup({ capabilities = capabilities })
 			lspconfig.lua_ls.setup({ capabilities = capabilities })
 			lspconfig.ts_ls.setup({ capabilities = capabilities })
