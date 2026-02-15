efc0b8031715fb93ec44857ff270bf6e28c69c8f
Drew Gingerich <drew@elsewhere.space>
2026-02-14 19:41:45 -0800

    nix: move from nixpkgs unstable to 25.11

--- flake.nix
+++ flake.nix
@@ -2,10 +2,10 @@
   description = "System configs";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/release-25.11";
     nix-darwin.url = "github:LnL7/nix-darwin";
     nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
-    home-manager.url = "github:nix-community/home-manager";
+    home-manager.url = "github:nix-community/home-manager/release-25.11";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
 
