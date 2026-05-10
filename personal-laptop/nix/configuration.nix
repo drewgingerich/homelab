c5c59b3506e88a996df22917f077cc2d6449bda0
Drew Gingerich <drew@elsewhere.space>
2026-05-09 22:30:36 -0700

    personal-laptop.nix: Add memtest86 to boot menu

--- personal-laptop/nix/configuration.nix
+++ personal-laptop/nix/configuration.nix
@@ -9,6 +9,7 @@
   ];
 
   boot.loader.systemd-boot.enable = true;
+  boot.loader.systemd-boot.memtest86.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
   boot.kernelPackages = pkgs.linuxPackages_latest;
   boot.extraModprobeConfig = ''
