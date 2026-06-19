ef1a8bcf66a62776752356075ac65fb16403e7e5
Drew Gingerich <drew@elsewhere.space>
2026-06-19 08:30:51 -0700

    personal-laptop.nix: fix bw-open bug

--- personal-laptop/nix/modules/home/bitwarden.nix
+++ personal-laptop/nix/modules/home/bitwarden.nix
@@ -1,55 +1,5 @@
 { pkgs, ... }:
 let
-  # newBwSession= pkgs.writeShellApplication {
-  #   name = "new-bw-session";
-  #   runtimeInputs = with pkgs; [libsecret bitwarden-cli fuzzel];
-  #   text = ''
-  #     BW_SESSION=$(secret-tool lookup bw-fuz bw-session-key || echo -n "")
-  #     export BW_SESSION
-  #     if ! bw unlock --check > /dev/null; then
-  #       bw_password=$(fuzzel --dmenu --password)
-  #       BW_SESSION=$(bw unlock "$bw_password" --raw)
-  #       echo -n "$BW_SESSION" | secret-tool store --label "bw-fuz" bw-fuz bw-session-key
-  #     fi
-  #     echo -n "$BW_SESSION"
-  #   '';
-  # };
-  #
-  # newBwSelect = pkgs.writeShellApplication {
-  #   name = "new-bw-select";
-  #   runtimeInputs = with pkgs; [newBwSession libsecret bitwarden-cli fuzzel];
-  #   text = ''
-  #     item_type=$1
-  #     BW_SESSION=$(new-bw-session)
-  #     export BW_SESSION
-  #     bw_items=$(bw list items | jq --raw-output '.[] | .id + "\t" + .name')
-  #     item_id=$(echo -n "$bw_items" | fuzzel --dmenu --hide-prompt --with-nth 2 --accept-nth 1 --minimal-lines)
-  #     echo -n "$item_id" | secret-tool store --label "bw-fuz login" bw-fuz "$item_type"
-  #   '';
-  # };
-  #
-  # newBwGet= pkgs.writeShellApplication {
-  #   name = "new-bw-get";
-  #   runtimeInputs = with pkgs; [libsecret bitwarden-cli];
-  #   text = ''
-  #     item_type="$1"
-  #     item_field="$2"
-  #     BW_SESSION=$(new-bw-session)
-  #     export BW_SESSION
-  #     item_id=$(secret-tool lookup bw-fuz "$item_type")
-  #     bw get "$item_field" "$item_id"
-  #   '';
-  # };
-  #
-  # newBwPaste = pkgs.writeShellApplication {
-  #   name = "new-bw-paste";
-  #   runtimeInputs = with pkgs; [libsecret bitwarden-cli];
-  #   text = ''
-  #     echo -n "$1" | wl-copy --paste-once
-  #     wtype -P XF86Paste
-  #   '';
-  # };
-   
   bwSync = pkgs.writeShellApplication {
     name = "bw-sync";
     runtimeInputs = with pkgs; [rbw];
@@ -95,7 +45,7 @@ let
     runtimeInputs = [pkgs.rbw bwSelect bwGet];
     text = ''
       bw-select login
-      bw-get login | jq -r '.data.uris[0]' | xargs xdg-open
+      bw-get login | jq -r '.data.uris[0].uri' | xargs xdg-open
     '';
   };
 
