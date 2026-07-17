ef62762e3f29877076feeac8d4088ba52e348777
Drew Gingerich <drew@elsewhere.space>
2026-07-16 20:24:35 -0700

    media-server.nix: import home-manager flake module

--- media-server/nix/flake-part.nix
+++ media-server/nix/flake-part.nix
@@ -3,6 +3,7 @@
   flake.nixosConfigurations.dusty-media-server = inputs.nixpkgs.lib.nixosSystem {
     system = "x86_64-linux";
     modules = [
+      inputs.home-manager.nixosModules.home-manager
       inputs.self.nixosModules.autorestic
       inputs.self.nixosModules.cliTools
       inputs.self.nixosModules.containers
