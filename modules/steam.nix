bf329763050dafeb1d46b5fd1a68a946bb61e9a4
Drew Gingerich <drew@elsewhere.space>
2026-09-06 23:33:15 -0700

    nix: add Steam supporting tools

--- modules/steam.nix
+++ modules/steam.nix
@@ -1,6 +1,13 @@
+{ ... }:
 {
-  flake.nixosModules.steam = {
-    programs.steam.enable = true;
+  flake.nixosModules.steam = { pkgs, ... }: {
+    programs.steam = {
+      enable = true;
+      extraCompatPackages = with pkgs; [
+        proton-ge-bin
+      ];
+      protontricks.enable = true;
+    };
     programs.gamemode.enable = true;
   };
 }
